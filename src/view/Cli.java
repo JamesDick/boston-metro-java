ffa35d82d45412308aa85b181759cbecc6db3bf0
miguel <miguel.pacheco.2018@uni.strath.ac.uk>
2021-03-01 19:44:39 +0000

    Basic command line interface

--- src/view/Cli.java
+++ src/view/Cli.java
@@ -7,44 +7,64 @@ public class Cli {
         prompt();
     }
 
+    /***
+     * Prompts the user to choose an option
+     */
     public void prompt() {
         Scanner in = new Scanner(System.in);
         boolean run = true;
+
         while (run) {
+            System.out.println();
+            System.out.print("Type an option: ");
             switch (in.nextLine()) {
-                case "1":
+                case "1" -> {
                     resetConsole();
                     System.out.println("Option 1 chosen");
-                    break;
-                case "2":
+                }
+                case "2" -> {
                     resetConsole();
                     System.out.println("Option 2 chosen");
-                    break;
-                case "0":
-                    resetConsole();
+                }
+                case "0" -> {
                     System.out.println("Exiting...");
                     run = false;
-                    break;
+                }
+                default -> {
+                    resetConsole();
+                    System.out.println("Error: invalid input.");
+                }
             }
         }
     }
-        public void resetConsole(){
-            try
-            {
-                final String os = System.getProperty("os.name");
 
-                if (os.contains("Windows")) {
-                    Runtime.getRuntime().exec("cls");
-                }
-                else {
-                    Runtime.getRuntime().exec("clear");
-                }
+
+    /***
+     * Clears the console
+     */
+    public void resetConsole(){
+        for(int i = 0; i < 60; i++) System.out.println();
+        try{
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            }else{
+                System.out.print("\033[H\033[2J");
             }
-            catch (final Exception e)
-            {
-                //  Exception here
-            }
+        }catch (final Exception e){
+            e.printStackTrace();
         }
+        printOptions();
+        System.out.println();
+    }
 
-
+    /***
+     * Prints a list of options
+     */
+    public void printOptions(){
+        System.out.println("Menu:");
+        System.out.println("0. Exit");
+        System.out.println("1. Option 1");
+        System.out.println("2. Option 2");
+    }
 }
