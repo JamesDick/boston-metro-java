b072ff804e1f729520e841cbb66f68e9aef81e69
James Dick <slightlyjames@gmail.com>
2021-03-01 18:40:52 +0000

    Implemented the button handlers in the controller package.

--- src/controller/ClearBtnListener.java
+++ src/controller/ClearBtnListener.java
@@ -1,4 +1,19 @@
 package controller;
 
-public class ClearBtnListener {
+import view.IView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClearBtnListener implements ActionListener {
+	IView view;
+	
+	public ClearBtnListener(IView view) {
+		this.view = view;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		view.exit();
+	}
 }
--- src/controller/FindRouteBtnListener.java
+++ src/controller/FindRouteBtnListener.java
@@ -1,4 +1,27 @@
 package controller;
 
-public class FindRouteBtnListener {
+import model.IModel;
+import multigraph.Station;
+import view.IView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+public class FindRouteBtnListener implements ActionListener {
+	IModel model;
+	IView view;
+	
+	public FindRouteBtnListener(IModel model, IView view) {
+		this.model = model;
+		this.view = view;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		int src = view.selectedSrcId();
+		int dest = view.selectedDestId();
+		List<Station> route = model.findRoute(src, dest);
+		view.displayRoute(route);
+	}
 }
