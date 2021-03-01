abece4e710431a7de46c5c1313528c238806ba7e
Naithan McNeil MCS2018 <naithan.mcneil.2018@uni.strath.ac.uk>
2021-03-01 18:58:20 +0000

    Node's basic stuff

--- src/multigraph/Node.java
+++ src/multigraph/Node.java
@@ -1,4 +1,14 @@
 package multigraph;
 
 public class Node {
+    public int id;
+    public String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
