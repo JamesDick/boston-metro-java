7939c8471c5bb6cf8b5c25d66cebfe02421dd658
Naithan McNeil MCS2018 <naithan.mcneil.2018@uni.strath.ac.uk>
2021-03-01 19:10:36 +0000

    Graph's basic stuff

--- src/multigraph/Graph.java
+++ src/multigraph/Graph.java
@@ -1,4 +1,16 @@
 package multigraph;
 
+import java.util.Map;
+
 public class Graph {
+    public Map<nodeId, N> nodes;
+    public List<E> edges;
+
+    public boolean addNode(N node) {
+        return false;
+    }
+
+    public boolean addEdge(E edge) {
+        return false;
+    }
 }
