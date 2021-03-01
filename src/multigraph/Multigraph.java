bd54dd4bf9fd02b84ddb2879a4cfa7c638408b5b
Naithan McNeil MCS2018 <naithan.mcneil.2018@uni.strath.ac.uk>
2021-03-01 19:11:05 +0000

    Multigraph's basic stuff

--- src/multigraph/Multigraph.java
+++ src/multigraph/Multigraph.java
@@ -1,4 +1,26 @@
 package multigraph;
 
+import java.util.List;
+import java.util.Map;
+
 public class Multigraph {
+    public Map<nodeId, N> nodes;
+    public List<E> edges;
+
+    public boolean addNode(s Station) {
+        return false;
+    }
+
+    public boolean addEdge(r Rail) {
+        return false;
+    }
+
+    public List<Station> findRoute(int srcId, int destId) {
+        return null;
+    }
+
+    public List<Station> getStations() {
+        return null;
+    }
 }
+
