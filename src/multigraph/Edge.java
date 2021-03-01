00365a71af8eae32ef91f0a695c79092c08855f7
Naithan McNeil MCS2018 <naithan.mcneil.2018@uni.strath.ac.uk>
2021-03-01 18:58:52 +0000

    Edge's basic stuff

--- src/multigraph/Edge.java
+++ src/multigraph/Edge.java
@@ -1,4 +1,14 @@
 package multigraph;
 
 public class Edge {
+    public int srcId;
+    public int destId;
+
+    public int getSrcId() {
+        return srcId;
+    }
+
+    public int getDestId() {
+        return destId;
+    }
 }
