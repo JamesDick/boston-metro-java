1e7986a6a1e0ac86d46edfbcf92ef152c5460cb3
James Dick <slightlyjames@gmail.com>
2021-03-01 18:33:51 +0000

    Added IView methods and added stub implementations to View

--- src/view/IView.java
+++ src/view/IView.java
@@ -1,4 +1,13 @@
 package view;
 
+import multigraph.*;
+
+import java.util.List;
+
 public interface IView {
+	public int selectedSrcId();
+	public int selectedDestId();
+	public void displayRoute(List<Station> route);
+	public void clearRoute();
+	public void exit();
 }
--- src/view/View.java
+++ src/view/View.java
@@ -1,4 +1,41 @@
 package view;
 
-public class View {
+import model.IModel;
+import multigraph.Station;
+
+import java.util.List;
+
+public class View implements IView {
+	
+	private GUI gui;
+	private IModel model;
+	
+	public View(IModel model) {
+		this.model = model;
+	}
+	
+	@Override
+	public int selectedSrcId() {
+		return 0;
+	}
+	
+	@Override
+	public int selectedDestId() {
+		return 0;
+	}
+	
+	@Override
+	public void displayRoute(List<Station> route) {
+	
+	}
+	
+	@Override
+	public void clearRoute() {
+	
+	}
+	
+	@Override
+	public void exit() {
+	
+	}
 }
