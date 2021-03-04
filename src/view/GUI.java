ffd9fd2fe519a88ead9148716d77d31610b4d35c
Emily Carr MCS2018 <emily.carr.2018@uni.strath.ac.uk>
2021-03-04 21:46:15 +0000

    Created border and main class for GUI.

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -1,4 +1,27 @@
 package view;
 
-public class GUI {
+import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI extends JFrame {
+
+    private JFrame frame;
+
+    public GUI() {
+        makeFrame();
+    }
+
+    private void makeFrame() {
+        frame = new JFrame("Boston Metro System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 750);
+        frame.setLocationRelativeTo(null);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
 }
