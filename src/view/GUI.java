ba9f6e9290922b6b185f274cd6d00521da100441
Emily <emily.carr.2018@uni.strath.ac.uk>
2021-03-04 20:07:51 +0000

    GUI outline created

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -1,4 +1,19 @@
 package view;
 
-public class GUI {
+import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI extends JFrame {
+
+    private JFrame frame;
+
+    private void makeFrame() {
+        frame = new JFrame("Boston Metro System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 750);
+        frame.setLocationRelativeTo(null);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setVisible(true);
+    }
 }
