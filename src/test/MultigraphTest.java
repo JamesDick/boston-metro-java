8126e6653209a8d525486df291adf1453368746c
Enrique <enrique.martin-ocana.2018@uni.strath.ac.uk>
2021-03-08 19:59:21 +0000

    Added tests for addNode and addEdge

--- src/test/MultigraphTest.java
+++ src/test/MultigraphTest.java
@@ -1,5 +1,9 @@
 package test;
 
+import main.multigraph.Graph;
+import main.multigraph.Multigraph;
+import main.multigraph.Rail;
+import main.multigraph.Station;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,13 +11,40 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MultigraphTest {
-	
+	private Multigraph graph;
+	private Station statOne;
+	private Station statOne_rep;
+	private Station statTwo;
+	private Rail railOne;
+	private Rail railTwo;
+	private Rail railThree;
+	private Rail railFour;
+
+
 	@BeforeEach
 	void setUp() {
+		graph = new Multigraph();
+		statOne = new Station(1,"One");
+		statOne_rep = new Station(1,"One");
+		statTwo = new Station(2, "Two");
+		railOne = new Rail(1,2,"Good");
+		railTwo = new Rail(1,3,"Bad");
+		railThree = new Rail(3,2,"Bad");
+		railFour = new Rail(3,3,"Bad");
+
 	}
 	
 	@AfterEach
 	void tearDown() {
+		graph = null;
+		statOne = null;
+		statOne_rep = null;
+		statTwo = null;
+		railOne = null;
+		railTwo = null;
+		railThree = null;
+		railFour = null;
+		assertNull(graph);
 	}
 	
 	/*
@@ -23,6 +54,8 @@ class MultigraphTest {
 	*/
 	@Test
 	void addNode() {
+		assertTrue(graph.addNode(statOne));
+		assertFalse(graph.addNode(statOne_rep));
 	}
 	
 	/*
@@ -32,6 +65,12 @@ class MultigraphTest {
 	*/
 	@Test
 	void addEdge() {
+		graph.addNode(statOne);
+		graph.addNode(statTwo);
+		assertTrue(graph.addEdge(railOne));
+		assertFalse(graph.addEdge(railTwo));
+		assertFalse(graph.addEdge(railThree));
+		assertFalse(graph.addEdge(railFour));
 	}
 	
 	/*
