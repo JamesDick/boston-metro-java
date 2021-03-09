9c140b30e37d4e0617ded9ce71559af9ec660b28
Emily <emily.carr.2018@uni.strath.ac.uk>
2021-03-09 17:28:28 +0000

    ModelTest: -Added to setUp and tearDown -Created a test to see if the model gets created

--- src/test/ModelTest.java
+++ src/test/ModelTest.java
@@ -1,22 +1,40 @@
 package test;
 
-
+import main.model.Model;
+import main.multigraph.Multigraph;
+import main.multigraph.Rail;
+import main.multigraph.Station;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ModelTest {
-	
+
+	private Model model;
+
 	@org.junit.jupiter.api.BeforeEach
 	void setUp() {
+		model = new Model();
 	}
-	
+
 	@org.junit.jupiter.api.AfterEach
 	void tearDown() {
+		model = null;
 	}
-	
+
+	//check if model has been created
+	@org.junit.jupiter.api.Test
+	void testCreate() {
+		assertNotNull(model);
+	}
+
 	@org.junit.jupiter.api.Test
 	void findRoute() {
+
+
 	}
-	
+
 	@org.junit.jupiter.api.Test
 	void getStations() {
 	}
