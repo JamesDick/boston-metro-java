82d3fa3606c5bd2b9ef7feeeda9abf081f82bfb1
Naithan McNeil MCS2018 <naithan.mcneil.2018@uni.strath.ac.uk>
2021-03-09 16:14:52 +0000

    BadFileException Comment

--- src/main/model/BadFileException.java
+++ src/main/model/BadFileException.java
@@ -1,5 +1,9 @@
 package main.model;
 
+//When reading in the text file we use this class to
+// get a clearer message of what went wrong when reading
+// in (even though we assume/know the supplied text file
+// should parse)
 public class BadFileException extends Exception{
     public BadFileException(String message){
         System.out.println("Error: " + message);
