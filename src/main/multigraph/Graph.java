2ddf2c4e569c0abee236aee74026d8666f093e6a
James Dick <slightlyjames@gmail.com>
2021-03-09 23:43:48 +0000

    Added a couple of sample javadoc comments in the Graph class for reference on how that's supposed to look. These are easy to do in IntelliJ, just lay down a /** then hit enter and it'll generate the whole thing for you, you just fill in the descriptions of what the method does and what the parameters are/what the method returns.

--- src/main/multigraph/Graph.java
+++ src/main/multigraph/Graph.java
@@ -5,16 +5,25 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 
+/**
+ * Represents a Graph containing Nodes and Edges.
+ * @param <N>
+ * @param <E>
+ */
 public abstract class Graph<N extends Node, E extends Edge> {
     protected Map<Integer, N> nodes;
     protected List<E> edges;
-
+    
     public Graph() {
         nodes = new HashMap<>();
         edges = new ArrayList<>();
     }
 
-    //Checks if node already exists and if not, adds the new node
+    /** Adds a Node to the Graph.
+     * Nodes may not contain an ID which is already present in the Graph.
+     * @param n The Node to be added.
+     * @return true if the Node was added to the Graph, otherwise false.
+     */
     public boolean addNode(N n) {
         if (nodes.containsKey(n.getId())) {
             return false;
@@ -23,8 +32,13 @@ public abstract class Graph<N extends Node, E extends Edge> {
         nodes.put(n.getId(), n);
         return true;
     }
-
-    //Checks if edge already exists and if not, adds the new edge
+    
+    /**
+     * Adds an Edge to the Graph.
+     * Edges must contain Nodes which already exist in the Graph.
+     * @param e The Edge to be added.
+     * @return true if the Edge was added to the Graph, otherwise false.
+     */
     public boolean addEdge(E e) {
         if (!nodes.containsKey(e.getSrc()) || !nodes.containsKey(e.getDest())) {
             return false;
