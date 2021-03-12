a2fcbed87ba698dfbeebe8d39a498c494555b2e6
Miguel Pacheco <miguel.pacheco.2018@uni.strath.ac.uk>
2021-03-12 12:32:06 +0000

    Since only one controller can be loaded per FXML file I've compiled all the "listeners" into a single class. There is a way to split FXML files into several, which would allow different controller classes, but needs to be investigated.
    As a wee experiment, the controller handles clicks in the map image and translate them into station selection. So far I'm hard coding the pixel boundaries of each station (by clicking on the map and seeing the console output), but might be a simpler way to do this. Anyway shouldn't take more than an hour to add them all.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,30 @@
 package main;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import main.model.Model;
-import main.multigraph.Station;
-import main.view.View;
+import main.controller.Controller;
 
-public class Main {
+public class Main extends Application{
     public static void main(String[] args) {
-        Model model = new Model();
-        
-        //TEST
-        for(Station s : model.findRoute(9, 60)) {
-            System.out.println(s.getName());
-        }
-        
-        View view = new View(model);
+        launch();
     }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Model model = new Model();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/MainView.fxml"));
+        loader.setController(new Controller(model));
+        Parent root = loader.load();
+
+        stage.setScene(new Scene(root));
+        stage.setWidth(1150);
+        stage.setHeight(630);
+//        stage.setResizable(false);
+        stage.show();
+    }
+
 }
