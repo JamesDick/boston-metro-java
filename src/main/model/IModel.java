447e795a13b3106e4cbc17ce54e8df6a719a9dd2
Emily <emily.carr.2018@uni.strath.ac.uk>
2021-03-24 15:56:08 +0000

    Added Method Specifications for: -Model -IModel -Action Listeners

--- src/main/model/IModel.java
+++ src/main/model/IModel.java
@@ -11,6 +11,13 @@ public interface IModel {
     /**
      * Finds a route between two specified Stations in the Metro.
      *
+     * Requires: Multigraph is populated with Nodes and Edges such that Nodes indicated by src
+     *           and dest exist in the Graph, and that there are sufficient Edges to form a route between them.
+     *
+     * Effects:	A List of Stations is returned indicating the Route between the src and dest Stations.
+     *
+     * Modifies: n/a
+     *
      * @param src  The source Station at which we will start.
      * @param dest The destination Station we wish to find a route to.
      * @return A list of Stations connecting the source and destination Stations if found, otherwise null.
@@ -19,6 +26,13 @@ public interface IModel {
 
     /**
      * Gets a list of all the Stations in the Metro.
+     *
+     * Requires: Multigraph is populated such that every station within the Boston Metro System has been added to the
+     *           Multigraph.
+     *
+     * Effects:	A List of all the Stations in the Metro.
+     *
+     * Modifies:	n/a
      *
      * @return A list of all the Stations in the Metro.
      */
